8bb8a8e4d44482302bc6c8413b39ffddf274c409
Yannis Piot Pilot <yannis@piotpilot.eu>
2020-11-08 15:40:31 +0100

    WIP

--- server/libs/alert.js
+++ server/libs/alert.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import webhook from 'webhook-discord';
 
 export default class Alert {
   /**
@@ -26,14 +27,29 @@ export default class Alert {
    * @param {string} message
    */
   discord(type, message) {
+ 
+    const Hook = new webhook.Webhook("URL");
+     
+var ts_hms = new Date();
+
+var dt =
+    ts_hms.getFullYear() + '-' + 
+    ("0" + (ts_hms.getMonth() + 1)).slice(-2) + '-' + 
+    ("0" + (ts_hms.getDate())).slice(-2) + ' ' +
+    ("0" + ts_hms.getHours()).slice(-2) + ':' +
+    ("0" + ts_hms.getMinutes()).slice(-2) + ':' +
+    ("0" + ts_hms.getSeconds()).slice(-2);
+
+const msg = new webhook.MessageBuilder()
+                .setName("KiwiNetwork")
+                .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/France_road_sign_A14.svg/220px-France_road_sign_A14.svg.png")
+                .setTitle("YOLO LE TITRE")
+                .setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non augue facilisis, volutpat odio ultricies, euismod dui. Phasellus volutpat erat et magna faucibus, eget blandit augue blandit. Vestibulum vitae ultrices quam. Sed et mattis est, ut gravida urna. Pellentesque ac justo urna. Aenean et massa at est fermentum ultricies a vel erat. Aliquam commodo vitae odio placerat ornare. Morbi non fermentum sem. Nullam elementum fermentum iaculis. Suspendisse vel diam mauris. Curabitur tempus euismod ante eu vestibulum. Duis dictum ante id orci mattis efficitur. Vestibulum porttitor eros auctor diam fringilla fermentum. Nunc nibh nisl, porta non metus et, tempor varius velit. Sed dignissim vel mi sit amet sagittis.")
+                .setFooter(dt)
+                .setColor("#fc0317");
+Hook.send(msg);
+
     return false;
   }
 
-  /**
-   * @param {string} type
-   * @param {string} message
-   */
-  email(type, message, mail) {
-    return false;
-  }
 }
